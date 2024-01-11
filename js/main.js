c4ca8c1f5c359e14620ae02697b16ca4a6bdcc77
lorenzodisanto <lore.disanto95@gmail.com>
2024-01-11 17:05:50 +0100

    feat: add dom

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+const resultElement = document.getElementById("cards-box");
+
 for (let i = 1; i <= 100; i++) {
+  let result = "";
   if (i % 15 == 0) {
     console.log("FizzBuzz");
+    result = "FizzBuzz";
   } else if (i % 5 == 0) {
     console.log("Buzz");
+    result = "Buzz";
   } else if (i % 3 == 0) {
     console.log("Fizz");
+    result = "Fizz";
   } else {
     console.log(i);
+    result = i;
   }
+
+  const cardsElement = `<div class="cards p-3 m-3">${result}</div>`;
+  resultElement.innerHTML += cardsElement;
 }
