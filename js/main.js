47c43bf8765879febbc2686ce80a8c0207065d8a
lorenzodisanto <lore.disanto95@gmail.com>
2024-01-11 17:28:29 +0100

    feat: add colorclass

--- js/main.js
+++ js/main.js
@@ -2,20 +2,25 @@ const resultElement = document.getElementById("cards-box");
 
 for (let i = 1; i <= 100; i++) {
   let result = "";
+  let colorClass = "";
   if (i % 15 == 0) {
     console.log("FizzBuzz");
     result = "FizzBuzz";
+    colorClass = "cards-fizzbuzz";
   } else if (i % 5 == 0) {
     console.log("Buzz");
     result = "Buzz";
+    colorClass = "cards-buzz";
   } else if (i % 3 == 0) {
     console.log("Fizz");
     result = "Fizz";
+    colorClass = "cards-fizz";
   } else {
     console.log(i);
     result = i;
+    colorClass = "cards-base";
   }
 
-  const cardsElement = `<div class="cards p-3 m-3">${result}</div>`;
+  const cardsElement = `<div class="cards p-3 m-3 ${colorClass}">${result}</div>`;
   resultElement.innerHTML += cardsElement;
 }
